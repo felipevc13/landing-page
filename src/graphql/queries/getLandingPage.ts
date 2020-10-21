09166b5a07a023a3d014d319ff65981168cf7999
Felipe Vasques <felipevc13@gmail.com>
2020-10-21 09:40:35 -0300

    primeiro commit

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,44 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        url
+        alternativeText
+      }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      title
+      description
+      image {
+        alternativeText
+        url
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+    }
+  }
+`
+
+export default GET_LANDING_PAGE

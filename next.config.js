611864b166f6aeeaada73f11d6bb17b577835965
Felipe Vasques <felipevc13@gmail.com>
2020-10-23 16:35:03 -0300

    adicionando pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
 const withPlugins = require('next-compose-plugins')
+
 const optimizedImages = require('next-optimized-images')
 
 module.exports = withPlugins([optimizedImages])
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="theme-color" content="#06092B" />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <NextSeo
         title="React Avançado - Crie aplicações reais com NextJS, GraphQL e mais."
